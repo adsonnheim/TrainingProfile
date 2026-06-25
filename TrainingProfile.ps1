f99fd549e1fcc5aeeec57568ecdc01a4961122c1
Alan Sonnheim <alan.sonnheim@thermtech.net>
2026-06-25 14:53:46 -0500

    Add input checking, add correct name formatting, TODO: handle single word inputs

--- TrainingProfile.ps1
+++ TrainingProfile.ps1
@@ -1,3 +1,15 @@
+Function Format-Name() {
+    Param (
+        $Name
+    )
+
+    If (($Name -split ' ').Count -ne 2) {
+        Return $False
+    }
+
+    Return (($Name -split ' ', 2)[0].SubString(0, 1).ToUpper() + ($Name -split ' ', 2)[0].SubString(1, ($Name -split ' ', 2)[0].Length - 1).ToLower()) + " " + (($Name -split ' ', 2)[1].SubString(0, 1).ToUpper() + ($Name -split ' ', 2)[1].SubString(1, ($Name -split ' ', 2)[1].Length - 1).ToLower())
+}
+
 $EnvFilePath = "./.env.local"
 
 If (-not (Test-Path $EnvFilePath -PathType Leaf)) {
@@ -24,4 +36,22 @@ If (-not (Get-Module -ListAvailable -Name PnP.Powershell)) {
     Install-Module PnP.Powershell
 }
 
-Connect-PnPOnline $Env["SHAREPOINT_URL"] -Interactive -ClientId $Env["CLIENT_ID"]
+$FirstUser = "0"
+$SecondUser = "1"
+$Fails = 0
+
+While ($FirstUser -ne $SecondUser) {
+    If ($Fails -ne 0) {
+        Write-Host "ERROR: Names do not match" -ForegroundColor Red
+    }
+
+    $FirstInput = ((Read-Host "Enter the first and last name of the user you wish to create a training profile for") -replace '\s+', ' ').Trim()
+    $FirstUser = Format-Name($FirstInput)
+
+    $SecondInput = ((Read-Host "Re-type the first and last name of the user to verify") -replace '\s+', ' ').Trim()
+    $SecondUser = Format-Name($SecondInput)
+
+    $Fails++
+}
+
+$FirstUser
